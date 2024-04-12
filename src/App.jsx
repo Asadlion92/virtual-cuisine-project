b309f56d0e7926f31b4aa1c4dd42be0b5c32dee4
Jafar Asad <rafajdasa@gmail.com>
2024-04-12 07:53:20 +0300

    90% done with footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import HomePage from './pages/HomePage/HomePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
 
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Footer.css'
+import { BiSolidDish } from 'react-icons/bi'
+import { MdOutlineMail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <div className='footer-logo'>
+            <BiSolidDish className='footer-logo-img' />
+            <div className='footer-text'>Virtual Cuisine</div>
+        </div>
+        <div className="footer-content-container">
+            <div className="footer-left-content">
+                <h3>Start Exploring</h3>
+                <p>Browse our course offerings, discover new recipes, and join our community of passionate food lovers. The kitchen awaits - let's start cooking!</p>
+            </div>
+            <div className="footer-right-content">
+                <div className="footer-browse">
+                    <h3>Browse</h3>
+                    <a href="">Courses</a>
+                    <a href="">About Us</a>
+                    <a href="">Recipes</a>
+                    <a href="">Contact Us</a>
+                </div>
+                <div className="footer-courses">
+                    <h3>Courses</h3>
+                    <a href="">Chef Skills</a>
+                    <a href="">Cooking Essentials</a>
+                    <a href="">Nutrition In Cooking</a>
+                    <a href="">Plant-Based Cooking</a>
+                    <a href="">Teen's Kitchen</a>
+                    <a href="">International Cuisine</a>
+                </div>
+                <div className="footer-contact">
+                    <h3>Contact Us</h3>
+                    <div className='email-container'>
+                        <MdOutlineMail className='email'/>
+                        <div>virtualcusine@gmail.com</div>
+                    </div>
+                    <div className='phone-container'>
+                        <FaPhone className='phone'/>
+                        <div>1-800-555-5555</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
