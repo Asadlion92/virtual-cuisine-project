392a9bfc2823ab903c21571042bab50f8474a24e
Jafar Asad <rafajdasa@gmail.com>
2024-04-14 13:25:10 +0300

    completed courses page with better background image

--- src/components/CourseIcons/CourseIcons.jsx
+++ src/components/CourseIcons/CourseIcons.jsx
@@ -6,12 +6,6 @@ import { IoIosNutrition } from "react-icons/io";
 import { PiPlant } from "react-icons/pi";
 import { PiHamburger } from "react-icons/pi";
 import { IoEarthOutline } from "react-icons/io5";
-import courseIconOne from '../../images/course-icon1.png'
-import courseIconTwo from '../../images/course-icon2.png'
-import courseIconThree from '../../images/course-icon3.png'
-import courseIconFour from '../../images/course-icon4.png'
-import courseIconFive from '../../images/course-icon5.png'
-import courseIconSix from '../../images/course-icon6.png'
 import { Link } from 'react-router-dom';
 import sound from '../../assets/button-click.wav'
 
@@ -24,16 +18,9 @@ function CourseIcons() {
   return (
     <div className='courses-icon-container'>
         <div className="all-courses-icon-container">
-            <div className='course-icon'>
-                <div style={{
-                        width: '300px',
-                        height: '75vh',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${courseIconOne})`,
-                        borderRadius: '40px',
-                        filter: 'brightness(40%)'
-                    }}></div>
+
+            <div className='course-icon-cover'>
+                <div className='course-icon-background course-icon-background-1'></div>
                 <div className='course-icon-header'>
                     <h3>Chef Skills</h3>
                     <LuChefHat className='course-icon-img' />
@@ -41,16 +28,9 @@ function CourseIcons() {
                 <p>Master the skills and techniques that will take your culinary abilities to the next level and help you excel in the food industry.</p>
                 <Link><button className='course-icon-btn' onClick={play}>Register</button></Link>
             </div>
-            <div className='course-icon'>
-                <div style={{
-                        width: '300px',
-                        height: '75vh',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${courseIconTwo})`,
-                        borderRadius: '40px',
-                        filter: 'brightness(40%)'
-                    }}></div>
+
+            <div className='course-icon-cover'>
+                <div className='course-icon-background course-icon-background-2'></div>
                 <div className='course-icon-header'>
                     <h3>Cooking Essentials</h3>
                     <PiCookingPot className='course-icon-img' />
@@ -58,16 +38,9 @@ function CourseIcons() {
                 <p>Learn the essential skills and techniques that will lead to a lifetime of restaurant-quality cooking.</p>
                 <Link><button className='course-icon-btn' onClick={play}>Register</button></Link>
             </div>
-            <div className='course-icon'>
-                <div style={{
-                        width: '300px',
-                        height: '75vh',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${courseIconThree})`,
-                        borderRadius: '40px',
-                        filter: 'brightness(40%)'
-                    }}></div>
+
+            <div className='course-icon-cover'>
+                <div className='course-icon-background course-icon-background-3'></div>
                 <div className='course-icon-header'>
                     <h3>Nutrition in Cooking</h3>
                     <IoIosNutrition className='course-icon-img' />
@@ -75,16 +48,9 @@ function CourseIcons() {
                 <p>Master cookery and nutrition skills for health and well-being with this 20-week Accredited Online Nutrition Course.</p>
                 <Link><button className='course-icon-btn' onClick={play}>Register</button></Link>
             </div>
-            <div className='course-icon'>
-                <div style={{
-                        width: '300px',
-                        height: '75vh',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${courseIconFour})`,
-                        borderRadius: '40px',
-                        filter: 'brightness(40%)'
-                    }}></div>
+
+            <div className='course-icon-cover'>
+                <div className='course-icon-background course-icon-background-4'></div>
                 <div className='course-icon-header'>
                     <h3>Plant-Based Cooking</h3>
                     <PiPlant className='course-icon-img' />
@@ -92,16 +58,9 @@ function CourseIcons() {
                 <p>Learn the skills and techniques that will lead to a lifetime of plant-based and planet-friendly meals.</p>
                 <Link><button className='course-icon-btn' onClick={play}>Register</button></Link>
             </div>
-            <div className='course-icon'>
-                <div style={{
-                        width: '300px',
-                        height: '75vh',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${courseIconFive})`,
-                        borderRadius: '40px',
-                        filter: 'brightness(40%)'
-                    }}></div>
+
+            <div className='course-icon-cover'>
+                <div className='course-icon-background course-icon-background-5'></div>
                 <div className='course-icon-header'>
                     <h3>Teen's Kitchen</h3>
                     <PiHamburger className='course-icon-img' />
@@ -109,16 +68,9 @@ function CourseIcons() {
                 <p>Teen's Kitchen is a 6-week immersive and interactive online cookery course and certificate exclusively for young adults aged 13-18.</p>
                 <Link><button className='course-icon-btn' onClick={play}>Register</button></Link>
             </div>
-            <div className='course-icon'>
-                <div style={{
-                        width: '300px',
-                        height: '75vh',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundImage: `url(${courseIconSix})`,
-                        borderRadius: '40px',
-                        filter: 'brightness(40%)'
-                    }}></div>
+
+            <div className='course-icon-cover'>
+                <div className='course-icon-background course-icon-background-6'></div>
                 <div className='course-icon-header'>
                     <h3>International Cuisine</h3>
                     <IoEarthOutline className='course-icon-img' />
