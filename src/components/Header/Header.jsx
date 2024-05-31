08fbdb9be712030a5fe22f8827d4a5b8a4db1868
Jafar Asad <rafajdasa@gmail.com>
2024-05-31 06:49:01 +0300

    added sounds to the Header, project done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,13 @@ import {React, useState} from 'react'
 import './Header.css'
 import { Link, NavLink } from 'react-router-dom'
 import { BiSolidDish } from 'react-icons/bi'
+import sound from '../../assets/button-click.wav'
 
 function Header() {
+
+  const play = () => {
+    new Audio(sound).play()
+  }
 
   const [menuOpen, setMenuOpen] = useState(false)
 
@@ -11,7 +16,7 @@ function Header() {
     <div className='header-container'>
         <nav className='nav-container'>
           <div className="header-left-content">
-            <Link to='/'>
+            <Link to='/' onClick={play}>
               <BiSolidDish className='logo-img' />
               <div>Virtual Cuisine</div>
             </Link>
@@ -23,10 +28,10 @@ function Header() {
           </div>
           <div className='header-right-content'>
             <ul className={menuOpen ? "open" : ""} onClick={() => {setMenuOpen(!menuOpen)}}>
-              <li><NavLink to='/about'>About Us</NavLink></li>
-              <li><NavLink to='/courses'>Courses</NavLink></li>
-              <li><NavLink to='/recipes'>Recipes</NavLink></li>
-              <li><NavLink to='/contact'>Contact Us</NavLink></li>
+              <li><NavLink to='/about' onClick={play}>About Us</NavLink></li>
+              <li><NavLink to='/courses' onClick={play}>Courses</NavLink></li>
+              <li><NavLink to='/recipes' onClick={play}>Recipes</NavLink></li>
+              <li><NavLink to='/contact' onClick={play}>Contact Us</NavLink></li>
             </ul>
           </div>
         </nav>
